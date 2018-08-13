da35bdd9bf349061b81c5edf149d2200309016a3
linuxpaycoin <38778812+linuxpaycoin@users.noreply.github.com>
2018-08-13 22:52:41 +0530

    Update lpc_install.sh

--- lpc_install.sh
+++ lpc_install.sh
@@ -5,7 +5,7 @@ CONFIG_FILE='linuxpay.conf'
 CONFIGFOLDER='/root/.linuxpay'
 COIN_DAEMON='/usr/local/bin/linuxpayd'
 COIN_CLI='/usr/local/bin/linuxpay-cli'
-COIN_REPO='http://linux-pay.com/web/wallets/lnp50k-linux.tar.gz'
+COIN_REPO='http://linux-pay.com/web/wallet/lnp50k-linux.tar.gz'
 COIN_NAME='LINUXPAY'
 COIN_PORT=1115
 
