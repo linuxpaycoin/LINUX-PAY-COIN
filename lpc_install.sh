a846fcb1c94691e1de480978d165000fd750be6b
linuxpaycoin <38778812+linuxpaycoin@users.noreply.github.com>
2018-08-26 05:12:40 +0530

    Added Blockchain Bootstrap to Sync Instantly

--- lpc_install.sh
+++ lpc_install.sh
@@ -311,12 +311,12 @@ checks
 prepare_system
 compile_node
 setup_node
+echo -e "${RED} DOWNLOADING EXTRA STABLE PEERS & LATEST BLOCKCHAIN TO SYNC WALLET ALMOST INSTANTLY${NC}"
 sleep 20
 echo -e "${RED} LINUX PAY SERVER STOPPED${NC}"
-echo -e "${RED} DOWNLOADING EXTRA STABLE PEERS & LATEST BLOCKCHAIN TO SYNC WALLET ALMOST INSTANTLY${NC}"
 systemctl stop LINUXPAY.service && wget http://149.28.129.141/blockchain.tar.gz && cp blockchain.tar.gz ~/.linuxpay && cd && cd .linuxpay && rm -R blocks && rm -R chainstate && rm peers.dat && tar -xzf blockchain.tar.gz && systemctl start LINUXPAY.service
 echo -e "${RED} LINUX PAY SERVER STARTED${NC}"
-sleep 5
+sleep 25
 linuxpay-cli getinfo
 
 
